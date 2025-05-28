6d8b9a5768da61d058a1f356b6ddbc636cae8530
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-27 20:37:18 -0400

    Add editing context provider

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { AuthContextProvider } from "./contexts/Auth/AuthContextProvider.tsx";
+import { EditingContextProvider } from "./contexts/Editing/EditingContextProvider.tsx";
 
 createRoot(document.getElementById("root") as Element).render(
   <StrictMode>
     <AuthContextProvider>
-      <App />
+      <EditingContextProvider>
+        <App />
+      </EditingContextProvider>
     </AuthContextProvider>
   </StrictMode>
 );
