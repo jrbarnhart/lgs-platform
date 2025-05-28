a59a0a33edb8c4cdf65429fb64946821b5628535
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-27 20:11:07 -0400

    Add auth context and provider

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,37 @@
+import News from "./components/news/News";
+import Specials from "./components/specials/Specials";
+import StoreHours from "./components/storeHours/StoreHours";
+import { useAuthContext } from "./contexts/Auth/useAuthContext";
+
 function App() {
+  const { loggedIn, setLoggedIn } = useAuthContext();
+
+  const handleLoginButton = () => {
+    setLoggedIn((prev) => !prev);
+  };
+
+  const handleCommitButton = () => {
+    window.location.reload();
+  };
+
   return (
     <div>
-      <p>Hello World.</p>
+      <h1>Local Game Store</h1>
+      <nav>
+        <button type="button" onClick={handleLoginButton}>
+          {loggedIn ? "Logout" : "Login"}
+        </button>
+        {loggedIn && (
+          <button type="button" onClick={handleCommitButton}>
+            Commit Changes
+          </button>
+        )}
+      </nav>
+      <main>
+        <StoreHours />
+        <News />
+        <Specials />
+      </main>
     </div>
   );
 }
