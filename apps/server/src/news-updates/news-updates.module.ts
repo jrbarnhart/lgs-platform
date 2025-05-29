1968ad21bab243f7bba747079c62d716209d4fe4
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-29 05:08:50 -0400

    Add prisma service to news updates service and module

--- apps/server/src/news-updates/news-updates.module.ts
+++ apps/server/src/news-updates/news-updates.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NewsUpdatesService } from './news-updates.service';
 import { NewsUpdatesController } from './news-updates.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [NewsUpdatesController],
-  providers: [NewsUpdatesService],
+  providers: [NewsUpdatesService, PrismaService],
 })
 export class NewsUpdatesModule {}
--- apps/server/src/news-updates/news-updates.service.ts
+++ apps/server/src/news-updates/news-updates.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateNewsUpdateDto } from './dto/create-news-update.dto';
 import { UpdateNewsUpdateDto } from './dto/update-news-update.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class NewsUpdatesService {
+  constructor(private prisma: PrismaService) {}
+
   create(createNewsUpdateDto: CreateNewsUpdateDto) {
     return 'This action adds a new newsUpdate';
   }
 
-  findAll() {
-    return `This action returns all newsUpdates`;
+  async findAll() {
+    const updateCount = await this.prisma.newsUpdate.count();
+    return `This action returns all newsUpdates. Total: ${updateCount}`;
   }
 
   findOne(id: number) {
