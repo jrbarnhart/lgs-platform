7047d3212bb95e21f76b24d7125f1cccb1ea9349
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-29 20:21:43 -0400

    Split into entity, create and update dtos

--- packages/zodDto/src/dto/storeHour.ts
+++ packages/zodDto/src/dto/storeHour.ts
@@ -1,22 +1,28 @@
 import { z } from "zod";
 import { zDayInt, zId, zString64 } from "./sharedSchemas";
 
-export const storeHourDto = z.object({
+export const storeHourEntity = z.object({
   id: zId,
   dayOfWeek: zDayInt,
   openTime: zString64.optional(),
   closeTime: zString64.optional(),
   isClosed: z.boolean().optional(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
 });
 
-export const specialStoreHourDto = z.object({
-  id: zId,
-  date: z.date(),
-  description: zString64.optional(),
-  dayOfWeek: zDayInt,
-  openTime: zString64.optional(),
-  closeTime: zString64.optional(),
-  isClosed: z.boolean().optional(),
+export const createStoreHourDto = storeHourEntity.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
 });
 
-export type StoreHourDto = z.infer<typeof storeHourDto>;
+export const updateStoreHourDto = storeHourEntity
+  .omit({ id: true, createdAt: true, updatedAt: true })
+  .partial({
+    dayOfWeek: true,
+  });
+
+export type StoreHourEntity = z.infer<typeof storeHourEntity>;
+export type CreateStoreHourDto = z.infer<typeof createStoreHourDto>;
+export type UpdateStoreHourDto = z.infer<typeof updateStoreHourDto>;
