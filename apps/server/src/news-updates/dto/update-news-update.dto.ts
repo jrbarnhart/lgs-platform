4af654c241ecb49eff5101cc8440d305198cbba7
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-29 20:55:44 -0400

    Remove unused files

--- apps/server/src/news-updates/dto/update-news-update.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateNewsUpdateDto } from './create-news-update.dto';
-
-export class UpdateNewsUpdateDto extends PartialType(CreateNewsUpdateDto) {}
