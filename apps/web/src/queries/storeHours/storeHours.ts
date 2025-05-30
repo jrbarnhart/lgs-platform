8b97de71d7cf0746fd50e14d481dad298d92ea09
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-30 15:30:16 -0400

    Add fetch functions for store hours

---
+++ apps/web/src/queries/storeHours/storeHours.ts
@@ -0,0 +1,39 @@
+import type { StoreHourEntity } from "lgs-zod-dto";
+import { axiosClient } from "../axiosClient";
+import axios from "axios";
+
+export async function fetchStoreHours() {
+  try {
+    const storeHourQuery = await axiosClient.get<StoreHourEntity[]>(
+      "/store-hours"
+    );
+
+    return storeHourQuery.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.message);
+    } else {
+      throw new Error(
+        "An unexpected error occurred in /store-hours fetch function"
+      );
+    }
+  }
+}
+
+export async function fetchMonsterById(id: string | number) {
+  try {
+    const storeHourQuery = await axiosClient.get<StoreHourEntity>(
+      `/monsters/${id.toString()}`
+    );
+
+    return storeHourQuery.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.message);
+    } else {
+      throw new Error(
+        "An unexpected error occurred in /store-hours/:id fetch function"
+      );
+    }
+  }
+}
