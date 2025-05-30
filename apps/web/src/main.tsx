85450b6a7a10fe1d36ba7c92770f874ebdc7381a
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-30 16:16:45 -0400

    Add devtools for query and router

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -6,6 +6,7 @@ import { EditingContextProvider } from "./contexts/Editing/EditingContextProvide
 import { routeTree } from "./routeTree.gen.ts";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -29,6 +30,7 @@ if (!rootElement.innerHTML) {
         <EditingContextProvider>
           <QueryClientProvider client={queryClient}>
             <RouterProvider router={router} />
+            <ReactQueryDevtools initialIsOpen={false} />
           </QueryClientProvider>
         </EditingContextProvider>
       </AuthContextProvider>
