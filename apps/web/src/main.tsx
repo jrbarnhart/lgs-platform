9aa391165bffda56480e5f36c626259472e663b7
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-30 16:45:35 -0400

    Configure router for react query

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -8,8 +8,17 @@ import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+// React Query client
+const queryClient = new QueryClient();
+
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  context: { queryClient },
+  defaultPreload: "intent",
+  defaultPreloadStaleTime: 0,
+  scrollRestoration: true,
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
@@ -17,9 +26,6 @@ declare module "@tanstack/react-router" {
     router: typeof router;
   }
 }
-
-// React Query client
-const queryClient = new QueryClient();
 
 const rootElement = document.getElementById("root") as Element;
 if (!rootElement.innerHTML) {
