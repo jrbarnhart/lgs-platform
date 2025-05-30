f738a12de4d7b5181af134efb282d12369ebc1f0
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-30 14:44:08 -0400

    Add query client and dev tools

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,8 +1,10 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import EditUI from "./components/editUI/EditUI";
 import News from "./components/news/News";
 import Specials from "./components/specials/Specials";
 import StoreHours from "./components/storeHours/StoreHours";
 import { useAuthContext } from "./contexts/Auth/useAuthContext";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   const { loggedIn, setLoggedIn } = useAuthContext();
@@ -15,26 +17,31 @@ function App() {
     window.location.reload();
   };
 
+  const queryClient = new QueryClient();
+
   return (
-    <div>
-      <h1>Local Game Store</h1>
-      <nav>
-        <button type="button" onClick={handleLoginButton}>
-          {loggedIn ? "Logout" : "Login"}
-        </button>
-        {loggedIn && (
-          <button type="button" onClick={handleCommitButton}>
-            Commit Changes
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>Local Game Store</h1>
+        <nav>
+          <button type="button" onClick={handleLoginButton}>
+            {loggedIn ? "Logout" : "Login"}
           </button>
-        )}
-      </nav>
-      <main>
-        <StoreHours loggedIn={loggedIn} />
-        <News loggedIn={loggedIn} />
-        <Specials loggedIn={loggedIn} />
-      </main>
-      <EditUI />
-    </div>
+          {loggedIn && (
+            <button type="button" onClick={handleCommitButton}>
+              Commit Changes
+            </button>
+          )}
+        </nav>
+        <main>
+          <StoreHours loggedIn={loggedIn} />
+          <News loggedIn={loggedIn} />
+          <Specials loggedIn={loggedIn} />
+        </main>
+        <EditUI />
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
