a5b612cb685062cb9917a444f35b9d80fcd60b77
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-29 20:35:42 -0400

    Split into entity, create and update dtos

--- packages/zodDto/src/dto/newsUpdate.ts
+++ packages/zodDto/src/dto/newsUpdate.ts
@@ -1,11 +1,28 @@
 import { z } from "zod";
 import { zId, zString128 } from "./sharedSchemas";
 
-export const newsUpdateDto = z.object({
+export const newsUpdateEntity = z.object({
   id: zId,
   title: zString128,
   content: z.string().trim().min(1, "Must not be empty"),
   published: z.boolean().optional(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
 });
 
-export type NewsUpdateDto = z.infer<typeof newsUpdateDto>;
+export const createNewsUpdateDto = newsUpdateEntity.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+});
+
+export const updateNewsUpdateDto = newsUpdateEntity
+  .omit({ id: true, createdAt: true, updatedAt: true })
+  .partial({
+    title: true,
+    content: true,
+  });
+
+export type NewsUpdateEntity = z.infer<typeof newsUpdateEntity>;
+export type CreateNewsUpdateDto = z.infer<typeof createNewsUpdateDto>;
+export type UpdateNewsUpdateDto = z.infer<typeof updateNewsUpdateDto>;
--- packages/zodDto/src/dto/storeEvent.ts
+++ packages/zodDto/src/dto/storeEvent.ts
@@ -1,13 +1,31 @@
 import { z } from "zod";
 import { zId, zString128 } from "./sharedSchemas";
 
-export const storeEventDto = z.object({
+export const storeEventEntity = z.object({
   id: zId,
   title: zString128,
   description: z.string().trim().optional(),
   startDate: z.date(),
   endDate: z.date(),
   published: z.boolean().optional(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
 });
 
-export type StoreEventDto = z.infer<typeof storeEventDto>;
+export const createStoreEventDto = storeEventEntity.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+});
+
+export const updateStoreEventDto = storeEventEntity
+  .omit({ id: true, createdAt: true, updatedAt: true })
+  .partial({
+    title: true,
+    startDate: true,
+    endDate: true,
+  });
+
+export type StoreEventDto = z.infer<typeof storeEventEntity>;
+export type CreateStoreEventDto = z.infer<typeof createStoreEventDto>;
+export type UpdateStoreEventDto = z.infer<typeof updateStoreEventDto>;
