bda27bc97b17940da204305d4f72870c73820e9c
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-06-06 21:14:58 -0400

    Add very simple login route and service method

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async signIn({ username, password }: { username: string; password: string }) {
+    const adminPasswordHash = process.env.ADMIN_PASSWORD_HASH || 'notAHash';
+    const passwordMatches = await bcrypt.compare(password, adminPasswordHash);
+
+    if (username !== process.env.ADMIN_USERNAME || !passwordMatches) {
+      throw new UnauthorizedException();
+    }
+
+    return 'Authorized!';
+  }
+}
