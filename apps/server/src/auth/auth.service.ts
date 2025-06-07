ed34924febf8a419a40ec8ea98653eaf4ee0d0a8
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-06-06 21:44:41 -0400

    Add better null check for env and return jwt on success

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -1,18 +1,36 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
-import { LoginDto } from 'lgs-zod-dto';
+import { JwtEntity, LoginDto } from 'lgs-zod-dto';
 
 @Injectable()
 export class AuthService {
-  async signIn(loginDto: LoginDto) {
+  constructor(private jwtService: JwtService) {}
+
+  async signIn(loginDto: LoginDto): Promise<JwtEntity> {
     const { password, username } = loginDto;
-    const adminPasswordHash = process.env.ADMIN_PASSWORD_HASH || 'notAHash';
+    const adminPasswordHash = process.env.ADMIN_PASSWORD_HASH;
+    const adminUsername = process.env.ADMIN_USERNAME;
+
+    if (!adminPasswordHash || !adminUsername) {
+      throw new InternalServerErrorException(
+        'Server has not been configured for auth',
+      );
+    }
+
     const passwordMatches = await bcrypt.compare(password, adminPasswordHash);
 
-    if (username !== process.env.ADMIN_USERNAME || !passwordMatches) {
+    if (username !== adminUsername || !passwordMatches) {
       throw new UnauthorizedException();
     }
 
-    return 'Authorized!';
+    const payload = { sub: 1, username: adminUsername };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
